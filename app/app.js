d76ebbd4c3f75a61ae5deaaec651228b7fae2d2f
omry oz <omryoz@gmail.com>
2014-12-14 01:07:27 +0200

    update services

--- app/app.js
+++ app/app.js
@@ -5,5 +5,16 @@ angular.module('Ozmon', ['ngResource', 'ngRoute', 'ui.bootstrap', 'ui.date'])
       .when('/', {
         templateUrl: 'app/views/home/home.html',
         controller: 'HomeController'})
+        .when('/customers', {
+          controller: 'CustomersController',
+          templateUrl: 'app/views/customers.html'
+        })
+        .when('/orders/:customerId', {
+          controller: 'OrdersController',
+          templateUrl: 'app/views/orders.html'
+        })
       .otherwise({redirectTo: '/'});
   }]);
+
+
+
